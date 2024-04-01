c5679f44cb1108a3764b7023c082bc4e510cc3ed
AuthMe-Team <dev@authme.com>
2024-04-01 17:27:47 +0800

    generate sdk podspace

---
+++ generate_authme.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+version="$1"
+
+cat <<EOF > AuthMe.podspec.json
+{
+  "name": "AuthMe",
+  "version": "'$version'",
+  "summary": "AuthMe framework",
+  "description": "AuthMe Framework",
+  "homepage": "https://authme.com",
+  "license": "BSD",
+  "authors": {
+    "AuthMe-Team": "dev@authme.com"
+  },
+  "platforms": {
+    "ios": "13.0"
+  },
+  "source": {
+    "git": "https://github.com/AuthMe01/AuthMe_iOS.git",
+    "tag": "'$version'"
+  },
+  "ios": {
+    "vendored_frameworks": [
+      "AuthMe.xcframework",
+      "OpenSSL.xcframework",
+      "Algo.xcframework"
+    ],
+    "weak_frameworks": [
+      "AuthMe",
+      "OpenSSL",
+      "Algo"
+    ]
+  },
+  "libraries": "c++",
+  "swift_version": "4.2"
+}
+EOF
+
+rm -rf AuthMe/$version
+mkdir AuthMe/$version
+mv AuthMe.podspec.json AuthMe/$version
---
+++ generate_authme_ui.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+version="$1"
+
+cat <<EOF > AuthMeUI.podspec.json
+{
+  "name": "AuthMeUI",
+  "version": "'$version'",
+  "summary": "AuthMeUI framework",
+  "description": "AuthMeUI Framework",
+  "homepage": "https://authme.com",
+  "license": "BSD",
+  "authors": {
+    "AuthMe-Team": "dev@authme.com"
+  },
+  "platforms": {
+    "ios": "13.0"
+  },
+  "source": {
+    "git": "https://github.com/AuthMe01/AuthMeUI_iOS.git",
+    "tag": "'$version'"
+  },
+  "ios": {
+    "vendored_frameworks": "AuthMeUI.xcframework"
+  },
+  "dependencies": {
+    "Lottie": [
+      "~> 4.4"
+    ]
+  },
+  "swift_version": "4.2"
+}
+EOF
+
+rm -rf AuthMeUI/$version
+mkdir AuthMeUI/$version
+mv AuthMeUI.podspec.json AuthMeUI/$version
